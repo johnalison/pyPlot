f5e3adb7801d457adf9bff76f534558c7ec9990e
John Alison <johnda102@gmail.com>
2023-06-12 22:11:35 -0400

    Update color adding fill

--- pyPlot.py
+++ pyPlot.py
@@ -7,6 +7,8 @@ inFile = "test/testInputs.json"
 with open(inFile, 'r') as f:
   data = json.load(f)
 
+myyellow = (1, 1, 84./255)
+
 
 def printDir(data, level=0, maxLevel=-1):
     if not hasattr(data,"keys"):
@@ -43,12 +45,14 @@ def binCenters(bins):
     return 0.5*(bins[0:len(bins)-1] + bins[1:len(bins)] )
 
 
-def configAxis(inAxis, **kwargs):
+def configAxes(inAxis, **kwargs):
     if "ylabel" in kwargs: inAxis.set_ylabel(kwargs["ylabel"])
-    if "xlabel" in kwargs: inAxis.set_xlabel(kwargs["xlabel"])
     if "ylim"   in kwargs: inAxis.set_ylim  (kwargs["ylim"])
+    if "yscale" in kwargs: inAxis.set_yscale(kwargs["yscale"])
+
     if "xlim"   in kwargs: inAxis.set_xlim  (kwargs["xlim"])
-        
+    if "xlabel" in kwargs: inAxis.set_xlabel(kwargs["xlabel"])
+
 
 
 
@@ -59,7 +63,7 @@ def plotRatio(dataToPlot, **kwargs):
         print(f'Called plotRatio')
         print_parameters(**kwargs)
 
-    fig, axs = plt.subplots(2,1,figsize=(6,5), gridspec_kw={'height_ratios': [3, 1]})
+    fig, axs = plt.subplots(2,1,figsize=(7,5*4/3), gridspec_kw={'height_ratios': [3, 1]})
     plt.subplots_adjust(hspace=0.05)
 
 
@@ -68,43 +72,45 @@ def plotRatio(dataToPlot, **kwargs):
         dataToPlot[0]["n"],
         yerr = dataToPlot[0]["n"]**0.5,
         marker = '.',
-        fmt = '.k'
+        fmt = '.k',
+        linewidth=2,
+        markersize=10
     )
-    axs[0].step(binCenters(dataToPlot[1]["bins"]) ,dataToPlot[1]["n"] ,fillstyle="full",where='mid')    
+    axs[0].hist(binCenters(dataToPlot[1]["bins"]) ,bins=dataToPlot[1]["bins"], weights=dataToPlot[1]["n"],histtype="stepfilled",color=myyellow,ec="k",linewidth=1.5)
 
     axs[0].set_xticklabels([])
     axs[0].set_xticks([])
 
-    if "ylabel" in kwargs: axs[0].set_ylabel(kwargs["ylabel"])
-    if "ylim"   in kwargs: axs[0].set_ylim  (kwargs["ylim"])
-    if "xlim"   in kwargs:
-        axs[0].set_xlim  (kwargs["xlim"])
-        axs[1].set_xlim  (kwargs["xlim"])
-    
+    configAxes(axs[0],**kwargs)
+    axs[0].set_xlabel("")
+
+        
     #
     #  Plot Ratio
     #
     epsilon = 0#1e-4
     ratio = (dataToPlot[0]["n"]/(dataToPlot[1]["n"]+epsilon))
-    axs[1].plot(binCenters(dataToPlot[0]["bins"]), ratio)
 
-    xMin = dataToPlot[0]["bins"][0]
-    xMax = dataToPlot[0]["bins"][-1]
-    axs[1].plot([xMin,xMax],[1,1],"k:")
+    axs[1].set_yscale("linear")    
+    configAxes(axs[1],**kwargs)    
+    axs[1].set_yscale("linear")
     
-    
-    if "xlabel" in kwargs: axs[1].set_xlabel(kwargs["xlabel"])
     if "rlabel" in kwargs:
         axs[1].set_ylabel(kwargs["rlabel"])
     else:
         axs[1].set_ylabel("Ratio")
 
     if "rlim" in kwargs: axs[1].set_ylim(kwargs["rlim"])    
+    
+    axs[1].plot(binCenters(dataToPlot[0]["bins"]), ratio,c="k")
 
-        
-    #axs[1].plot([xMin,xMax],[1,1],"k:")
+    xMin = dataToPlot[0]["bins"][0]
+    xMax = dataToPlot[0]["bins"][-1]
+    axs[1].plot([xMin,xMax],[1,1],"k:")
+
+    
     plt.show()
-
+    return
         
     
 def plotHists(dataToPlot, **kwargs):
@@ -120,23 +126,36 @@ def plotHists(dataToPlot, **kwargs):
         return
 
         
-    fig, ax = plt.subplots(1,1,figsize=(6,5))
+    fig, ax = plt.subplots(1,1,figsize=(7,5))
         
-
     ax.errorbar(
         binCenters(dataToPlot[0]["bins"]),
         dataToPlot[0]["n"],
         yerr = dataToPlot[0]["n"]**0.5,
         marker = '.',
-        fmt = '.k'
+        fmt = '.k',
+        markersize=10
     )
     
     if len(dataToPlot) == 2:
-        ax.step(binCenters(dataToPlot[1]["bins"]) ,dataToPlot[1]["n"] ,where='mid')
 
-    configAxis(ax,**kwargs)
+        ax.hist(binCenters(dataToPlot[1]["bins"]) ,bins=dataToPlot[1]["bins"], weights=dataToPlot[1]["n"],histtype="stepfilled",color=myyellow, ec="k",linewidth=1.5)
+        #ax.hist(binCenters(dataToPlot[1]["bins"]) ,bins=dataToPlot[1]["bins"], weights=dataToPlot[1]["n"],histtype="step",color="k",linewidth=1.5)
+
+#    ax.errorbar(
+#        binCenters(dataToPlot[0]["bins"]),
+#        dataToPlot[0]["n"],
+#        yerr = dataToPlot[0]["n"]**0.5,
+#        marker = '.',
+#        fmt = '.k',
+#        markersize=10
+#    )
+
+        
+    configAxes(ax,**kwargs)
     
     plt.show()
+    return
 
 def checkList(inList, hasLength = 2):
     if not len(inList) == hasLength:
